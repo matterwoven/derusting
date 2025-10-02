594ef221f61455849411cabf60dbebeac019287d
Matthew Vargas <matthewvargas2013@gmail.com>
2025-10-01 17:17:27 -0700

    f

--- src/MapPractice.java
+++ src/MapPractice.java
@@ -15,15 +15,27 @@ public class MapPractice {
     // Get the value associated with a given key in the Map
     userScores.get("Bob");
     // Find the size (number of key/value pairs) of the Map
-    
+    System.out.println(userScores.size());
     // Replace the value associated with a given key (the size of the Map shoukld not change)
-
+    userScores.replace("Bob", 7);
     // Check whether the Map contains a given key
-
+    if (userScores.containsKey("Bob")){
+      System.out.println("Contains key");
+    }
+    else {
+      System.out.println("Does not contain key");
+    }
     // Check whether the Map contains a given value
-
+    if (userScores.containsValue("7")) {
+      System.out.println("Does contain 7");
+    }
+    else {
+      System.out.println("Does not contain 7");
+    }
     // Iterate over the keys of the Map, printing each key
-
+    for (String key : userScores.keySet()) {
+      System.out.println(key);
+    }
     // Iterate over the values of the map, printing each value
 
     // Iterate over the entries in the map, printing each key and value
